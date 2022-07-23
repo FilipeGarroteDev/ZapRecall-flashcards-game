06764869afda1cabad29fb5734e6b85a6d6da868
Filipe Garrote <filipe.garrote@gmail.com>
2022-07-23 02:14:07 -0300

    v8.0 - Add Finalização - Exibição de mensagem de acordo com desempenho

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,13 +4,42 @@ import sadEmoji from "../../assets/images/sad.png"
 import partyEmoji from "../../assets/images/party.png"
 
 export default function Footer({answeredIcons}){
-  
-  return (
-      <footer>
-        <h3>{answeredIcons.length}/4 CONCLUÍDOS</h3>
-        <div className="iconContainer">
-          {answeredIcons.map((icon) => <ion-icon name={icon}></ion-icon>)}
-        </div>
-      </footer>
-  )
+    {if (answeredIcons.length < 4){
+      return (
+        <footer>
+          <h3>{answeredIcons.length}/4 CONCLUÍDOS</h3>
+          <div className="iconContainer">
+            {answeredIcons.map((icon) => <ion-icon name={icon}></ion-icon>)}
+          </div>
+        </footer>
+      )
+    } else {
+      const correctAnswers = answeredIcons.filter(icon => icon === "checkmark-circle")
+      return(correctAnswers.length === 4 ?
+        <footer>
+          <div>
+            <img src={partyEmoji}/>
+            <h4>Parabéns!</h4>
+          </div>
+          <span>Você não esqueceu de nenhum flashcard!</span>
+          <h3>{answeredIcons.length}/4 CONCLUÍDOS</h3>
+          <div className="iconContainer">
+            {answeredIcons.map((icon) => <ion-icon name={icon}></ion-icon>)}
+          </div>
+        </footer>
+      :
+        <footer>
+          <div>
+            <img src={sadEmoji}/>
+            <h4>Putz...</h4>
+          </div>
+          <span>Ainda faltam alguns... Mas não desanime!</span>
+          <h3>{answeredIcons.length}/4 CONCLUÍDOS</h3>
+          <div className="iconContainer">
+            {answeredIcons.map((icon) => <ion-icon name={icon}></ion-icon>)}
+          </div>
+        </footer>)
+    }}
+      
+
 }
